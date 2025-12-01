21a7482d480991b46e6cfeba24b719b938a7052e
Matin Maleki <matin.mlk@gmail.com>
2025-11-30 22:19:11 -0500

    Commit all changes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,11 +19,10 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'matinmlk', // Usually your GitHub org/user name.
-  projectName: 'hemihex', // Usually your repo name.
+  projectName: 'hemihex-docs', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
 
